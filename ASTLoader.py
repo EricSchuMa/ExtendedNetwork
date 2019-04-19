46692bd6722e6438cf6913c85347c24508a3adf4
Max Schumacher <max.schumacher12@googlemail.com>
2019-04-19 20:57:54 +0200

    Implemented sequential data processing with generator object

--- ASTLoader.py
+++ ASTLoader.py
@@ -5,54 +5,77 @@ import random
 
 
 class ASTLoader(object):
-    def __init__(self, train_source, num_samples):
+    """Handles access of data and provides functionality to build the vocabulary space
+    """
+    def __init__(self, train_source):
+        """
+        Args:
+            self.word2_index_dict: dictionary that maps words to indices
+            self._train_source: variable that stores the path to the file source
+            self._train_file: inital file stream
+            self._sample_index: integer that handles the indexing of AST Nodes
+            self.index_2_word_map: reverse dictionary that is used to map the indices back to the original vocabulary
+            self._line_generator: iterable generator object that iterates over individual ASTs
+            self._current_line: list containing the Nodes of an AST that is processed next
+
+        Params:
+            train_source: path to the source file for the ASTs
+        """
+        self.word2_index_dict = {}
         self._train_source = train_source
-        self._num_samples = num_samples
+        self._train_file = open(train_source)
         self._sample_index = 0
-        self._data_index = 0
-        self.types_train = None
-        self.data = None
-        self.index2_word_dict = None
+        self.index_2_word_map = self.index_2_word_map()
+        self._line_generator = self.new_line_generator()
+        self._current_line = next(self._line_generator)
 
-    def load_types(self):
-        """loads types into training self.types_train"""
-        # open source file
-        _file_train = open(self._train_source)
+    def new_line_generator(self):
+        """Accesses the file stream and iterate over individual ASTs. Starts from the beginning of the file
+        the end of file is reached
 
-        # read lines and format data
-        data_train = [json.loads(_file_train.readline())
-                      for i in range(self._num_samples[0])]
+        Yields: the next AST
+        """
+        while True:
+            for line in self._train_file:
+                yield(self.get_type(line))
+            # reload file if we reached the end
+            self._train_file.close()
+            self._train_file = open(self._train_source)
 
-        # save the data
-        self.types_train = [[Node['type'] for Node in data_train[i]]
-                            for i in range(self._num_samples[0])]
+    def get_type(self, line):
+        """gets the type information of a given AST
 
-    def build_dataset(self):
-        """Builds the vocabulary and translates self.types_train
-           into the vocabulary space
+        Args:
+            line: string representing one program AST in the dataset
 
-            Returns:
-                data: An index representation of train_data
+        Returns:
+            list of Node types from the AST
+        """
 
-                index2_word_dict: Dictionary that translates
-                                  indices back to original words
-            """
-        word2_index_dict = {}
-        for sentence in self.types_train:
-            for word in sentence:
-                if word not in word2_index_dict:
-                    word2_index_dict[word] = len(word2_index_dict)
-        index2_word_dict = dict(zip(word2_index_dict.values(),
-                                    word2_index_dict.keys()))
+        # read line as json format
+        data = json.loads(line)
+        # return type information
+        words = [Node['type'] for Node in data]
+        return [self.word2_index_dict[word] for word in words]
 
-        # build the data
-        data = [[word2_index_dict.get(word, 0) for word in sentence]
-                for sentence in self.types_train]
+    def index_2_word_map(self):
+        """Builds the vocabulary space
 
-        self.data = data
-        self.index2_word_dict = index2_word_dict
+        Returns:
+            index2_word_dict: Dictionary that translates
+                              indices back to original words
+        """
+        with open(self._train_source) as file:
+            for line in file:
+                sentence = json.loads(line)
+                for word in sentence:
+                    if word['type'] not in self.word2_index_dict:
+                        self.word2_index_dict[word['type']] \
+                            = len(self.word2_index_dict)
+        index2_word_dict = dict(zip(self.word2_index_dict.values(),
+                                    self.word2_index_dict.keys()))
+        return index2_word_dict
 
-        return data, index2_word_dict
 
     def generate_batch(self, batch_size, skip_window, num_skips):
         """Function for generating a training batch for the skip-gram model
@@ -78,14 +101,15 @@ class ASTLoader(object):
         buffer = collections.deque(maxlen=span)
 
         # If we are at the end of an data point switch to the next one
-        if self._sample_index + span > len(self.data[self._data_index]):
-            self._data_index += 1
-
+        if self._sample_index + span > len(self._current_line):
+            self._current_line = next(self.new_line_generator())
+            # discard empty ASTs
+            while len(self._current_line) < span:
+                self._current_line = next(self.new_line_generator())
             # Start from first item if we reach the end of the data
-            self._data_index = self._data_index % len(self.data)
             self._sample_index = 0
 
-        buffer.extend(self.data[self._data_index]
+        buffer.extend(self._current_line
                       [self._sample_index: self._sample_index + span])
         self._sample_index += span
 
@@ -95,13 +119,15 @@ class ASTLoader(object):
             for j, context_word in enumerate(words_to_use):
                 batch[i * num_skips + j] = buffer[skip_window]
                 labels[i * num_skips + j, 0] = buffer[context_word]
-            if self._sample_index >= len(self.data[self._data_index]):
-                self._data_index += 1
-                self._data_index = self._data_index % len(self.data)
+            if self._sample_index >= len(self._current_line):
+                self._current_line = next(self.new_line_generator())
+                # discard empty ASTs
+                while len(self._current_line) < span:
+                    self._current_line = next(self.new_line_generator())
                 self._sample_index = 0
-                buffer.extend(self.data[self._data_index][0:span])
+                buffer.extend(self._current_line[0:span])
                 self._sample_index = span
             else:
-                buffer.append(self.data[self._data_index][self._sample_index])
+                buffer.append(self._current_line[self._sample_index])
                 self._sample_index += 1
         return batch, labels
