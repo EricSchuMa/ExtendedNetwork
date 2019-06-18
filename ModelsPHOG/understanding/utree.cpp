148b4c211976cedcbaa02a43b0f3f61c58189a86
Max Schumacher <max.schumacher12@googlemail.com>
2019-06-18 11:38:41 +0200

    Added PHOG-Model

---
+++ ModelsPHOG/understanding/utree.cpp
@@ -0,0 +1,63 @@
+//
+// Created by max on 17.06.19.
+//
+
+#include "phog/tree/tree.h"
+#include "base/stringset.h"
+
+#include "phog/dsl/tcond_language.h"
+#include "phog/dsl/tgen_program.h"
+#include "phog/model/model.h"
+
+DEFINE_string(training_data, "", "A file with the tree information.");
+DEFINE_string(evaluation_data, "", "A file with the training data.");
+DEFINE_string(tgen_program, "", "A file with a TGen program.");
+DEFINE_bool(is_for_node_type, true, "Whether the predictions are for node type (if false it is for node value).");
+DEFINE_int32(num_training_asts, 100000, "Maximun number of training ASTs to load.");
+DEFINE_int32(num_eval_asts, 50000, "Maximun number of evaluation ASTs to load.");
+
+
+int main(int argc, char** argv) {
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    StringSet ss;
+    TCondLanguage lang(&ss);
+    TGenProgram tgen_program;
+    TGen::LoadTGen(&lang, &tgen_program, FLAGS_tgen_program);
+
+    std::vector<TreeStorage> trees, eval_trees;
+    LOG(INFO) << "Loading training data...";
+    ParseTreesInFileWithParallelJSONParse(
+            &ss, FLAGS_training_data.c_str(), 0, FLAGS_num_training_asts, true, &trees);
+    LOG(INFO) << "Training data with " << trees.size() << " trees loaded.";
+
+    LOG(INFO) << "Loading evaluation data...";
+    ParseTreesInFileWithParallelJSONParse(
+            &ss, FLAGS_evaluation_data.c_str(), 0, FLAGS_num_eval_asts, true, &eval_trees);
+    LOG(INFO) << "Evaluation data with " << eval_trees.size() << " trees loaded.";
+
+    LOG(INFO) << "Training...";
+    TGenModel model(tgen_program, FLAGS_is_for_node_type);
+    for (size_t tree_id = 0; tree_id < trees.size(); ++tree_id) {
+        const TreeStorage &tree = trees[tree_id];
+        TCondLanguage::ExecutionForTree exec(&ss, &tree);
+        for (unsigned node_id = 0; node_id < tree.NumAllocatedNodes(); ++node_id) {
+            model.GenerativeTrainOneSample(model.start_program_id(), exec, FullTreeTraversal(&tree, node_id));
+            LOG_EVERY_N(INFO, FLAGS_num_training_asts * 100)
+                << "Training... (logged every " << FLAGS_num_training_asts * 100 << " samples).";
+        }
+    }
+    model.GenerativeEndTraining();
+    LOG(INFO) << "Training done.";
+
+//std::vector<std::string> predictions;
+    for (size_t tree_id = 0; tree_id < eval_trees.size(); ++tree_id) {
+        const TreeStorage& tree = eval_trees[tree_id];
+        TCondLanguage::ExecutionForTree exec(&ss, &tree);
+        for (unsigned node_id = 0; node_id < tree.NumAllocatedNodes(); ++node_id) {
+            FullTreeTraversal sample(exec.tree(), node_id);
+            TreeSlice slice(exec.tree(), node_id, !model.is_for_node_type());
+            std::cout << ss.getString(model.GetLabelAtPosition(model.start_program_id(), exec, sample, &slice));
+        }
+    }
+
+}
