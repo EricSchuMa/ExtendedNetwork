29f47f25bd58fc99db488b6cbe06b52a3507a823
Max Schumacher <max.schumacher12@googlemail.com>
2019-07-31 12:58:12 +0200

    type and value predictions with corresponding probs work now

--- ModelsPHOG/recommender/pred2file.cpp
+++ ModelsPHOG/recommender/pred2file.cpp
@@ -2,7 +2,7 @@
 // Created by max on 17.06.19.
 //
 
-#include "base/stringset.h"
+//#include "base/stringset.h"
 #include "phog/tree/tree.h"
 
 #include "phog/dsl/tcond_language.h"
@@ -11,84 +11,41 @@
 
 #include <fstream>
 
+#include <boost/algorithm/string.hpp>
+
 DEFINE_string(training_data, "", "A file with the tree information.");
-DEFINE_string(evaluation_data, "", "A file with the training data.");
+DEFINE_string(source_file, "", "A file with the tree information");
+DEFINE_string(target_file, "", "File for writing the predictions");
 DEFINE_string(tgen_program_types, "", "A file with a TGen program for types.");
+DEFINE_string(evaluation_data, "", "");
 DEFINE_string(tgen_program_values, "",
               "A file with a TGen program for values.");
 DEFINE_int32(num_training_asts, 100000,
              "Maximun number of training ASTs to load.");
-DEFINE_int32(num_eval_asts, 50000,
-             "Maximun number of evaluation ASTs to load.");
-DEFINE_string(save_path, "",
-              "Path to .json file for saving the predictions for each AST");
+DEFINE_int32(num_pred_asts, 50000,
+             "Maximun number of prediction ASTs to save.");
 
-int main(int argc, char **argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  StringSet ss;
-  TCondLanguage lang(&ss);
-  TGenProgram tgen_program_types, tgen_program_values;
-  TGen::LoadTGen(&lang, &tgen_program_types, FLAGS_tgen_program_types);
-  TGen::LoadTGen(&lang, &tgen_program_values, FLAGS_tgen_program_values);
+void write_pred_to_file(TGenModel& model_types, TGenModel& model_values,
+    StringSet &ss, int num_pred,
+    const std::string & source_file, const std::string & target_file) {
 
-  // TODO: write method for saving and loading trained models
-
-  // Loading Training data
-  std::vector<TreeStorage> trees, eval_trees;
-  LOG(INFO) << "Loading training data...";
-  ParseTreesInFileWithParallelJSONParse(&ss, FLAGS_training_data.c_str(), 0,
-                                        FLAGS_num_training_asts, true, &trees);
-  LOG(INFO) << "Training data with " << trees.size() << " trees loaded.";
-
-  // Training the HOG for types with predefined TCOND-program
-  LOG(INFO) << "Training types ...";
-  TGenModel model_types(tgen_program_types, true);
-  for (size_t tree_id = 0; tree_id < trees.size(); ++tree_id) {
-    const TreeStorage &tree = trees[tree_id];
-    TCondLanguage::ExecutionForTree exec(&ss, &tree);
-    for (unsigned node_id = 0; node_id < tree.NumAllocatedNodes(); ++node_id) {
-      model_types.GenerativeTrainOneSample(model_types.start_program_id(), exec,
-                                           FullTreeTraversal(&tree, node_id));
-      LOG_EVERY_N(INFO, FLAGS_num_training_asts * 100)
-          << "Training... (logged every " << FLAGS_num_training_asts * 100
-          << " samples).";
-    }
-  }
-  model_types.GenerativeEndTraining();
-  LOG(INFO) << "Training types done.";
-
-  // Training the HOG for values with predefined TCOND-program
-  LOG(INFO) << "Training values ...";
-  TGenModel model_values(tgen_program_values, false);
-  for (size_t tree_id = 0; tree_id < trees.size(); ++tree_id) {
-    const TreeStorage &tree = trees[tree_id];
-    TCondLanguage::ExecutionForTree exec(&ss, &tree);
-    for (unsigned node_id = 0; node_id < tree.NumAllocatedNodes(); ++node_id) {
-      model_values.GenerativeTrainOneSample(model_values.start_program_id(),
-                                            exec,
-                                            FullTreeTraversal(&tree, node_id));
-      LOG_EVERY_N(INFO, FLAGS_num_training_asts * 100)
-          << "Training... (logged every " << FLAGS_num_training_asts * 100
-          << " samples).";
-    }
-  }
-  model_values.GenerativeEndTraining();
-  LOG(INFO) << "Training values done.";
-
+  std::vector<TreeStorage> eval_trees;
   LOG(INFO) << "Loading evaluation data...";
-  ParseTreesInFileWithParallelJSONParse(&ss, FLAGS_evaluation_data.c_str(), 0,
-                                        FLAGS_num_eval_asts, true, &eval_trees);
+  ParseTreesInFileWithParallelJSONParse(&ss, source_file.c_str(), 0,
+                                        FLAGS_num_pred_asts, true, &eval_trees);
   LOG(INFO) << "Evaluation data with " << eval_trees.size() << " trees loaded.";
 
-  TreeNode node;
+  // open target file for writing predictions
   std::ofstream predictions;
-  std::cout << FLAGS_save_path;
-  predictions.open(FLAGS_save_path);
+  predictions.open(target_file);
+
+  TreeNode node;
   for (size_t tree_id = 0; tree_id < eval_trees.size(); ++tree_id) {
     predictions << "[";
     const TreeStorage &tree = eval_trees[tree_id];
     TCondLanguage::ExecutionForTree exec(&ss, &tree);
     for (unsigned node_id = 0; node_id < tree.NumAllocatedNodes(); ++node_id) {
+
       // same tree used for types and values
       FullTreeTraversal sample(exec.tree(), node_id);
 
@@ -98,69 +55,50 @@ int main(int argc, char **argv) {
       TreeSlice slice_values(exec.tree(), node_id,
                              !model_values.is_for_node_type());
 
-      // generate outputs for value and type
-      std::vector<std::pair<double, int const *>> output_types =
-          model_types.GetLabelDistribution(model_types.start_program_id(), exec,
-                                           sample, &slice_types, 1);
-      std::vector<std::pair<double, int const *>> output_values =
-          model_values.GetLabelDistribution(model_values.start_program_id(),
-                                            exec, sample, &slice_values, 1);
+      // generate outputs for value and type pair: <log_prob, label>
+      std::pair<double, int> type_pred =
+          model_types.GetBestLabelLogProb(model_types.start_program_id(),
+                                           exec, sample,
+                                           &slice_types);
+      std::pair<double, int> value_pred =
+          model_values.GetBestLabelLogProb(model_values.start_program_id(),
+              exec, sample,
+              &slice_values);
 
-      // write type-predictions to file
-      predictions << "{"
-                  << "\"" << "id" << "\":\"" << node_id << "\""
-                  << ",\"type\":[";
-      for (size_t j = 0; j < output_types.size(); ++j) {
-        std::string prediction = ss.getString(*output_types[j].second);
-        if (!prediction.empty()) {
-          if (j < output_types.size() - 1) {
-            predictions << "\"" << prediction << "\""
-                        << ",";
-          }
-          else {
-            predictions << "\"" << prediction << "\"";
-          }
-        }
-      }
 
-      // write value-predictions to file
-      predictions << "],\"value\":[";
-      for (size_t j = 0; j < output_values.size(); ++j) {
-        std::string prediction = ss.getString(*output_values[j].second);
-        if (!prediction.empty()) {
-          if (j < output_values.size() - 1) {
-            predictions << "\"" << prediction << "\""
-                        << ",";
-          }
-          else {
-            predictions << "\"" << prediction << "\"";
-          }
-        }
-      }
+      // write type-predictions
+      predictions << "{" << "\"type\":";
+      std::string type = ss.getString(type_pred.second);
+      predictions << "\"" << type << "\"";
 
-      //write probabilities for type
-      predictions << "],\"prob_types\":[";
-      for (size_t k = 0; k < output_types.size(); ++k) {
-        double prob = output_types[k].first;
-        if (k < output_types.size() - 1) {
-          predictions << prob << ",";
-        } else {
-          predictions << prob;
-        }
-      }
-      predictions << "]}";
+      // write prob for type
+      predictions << ",\"ptype\":";
+      predictions << "\"" << type_pred.first << "\"";
+      // write value-predictions
+      predictions << ",\"value\":";
 
-      //write probabilities for values
-      predictions << ",\"prob_values\":[";
-      for (size_t k = 0; k < output_values.size(); ++k) {
-        double prob = output_values[k].first;
-        if (k < output_values.size() - 1) {
-          predictions << prob << ",";
-        } else {
-          predictions << prob;
-        }
+      std::string value = ss.getString(value_pred.second);
+
+      // handle escapes
+      boost::replace_all(value, "\\", "\\\\");
+      boost::replace_all(value, "\"", "\\\"");
+      boost::replace_all(value, "\n", "\\n");
+
+      if(value_pred.second>0) {
+        predictions << "\"" << value << "\"";
       }
-      predictions << "]}";
+      else {
+          predictions << "\"" << "emptY" << "\"";}
+
+      // write prob for type
+      predictions << ",\"pvalue\":";
+      predictions << "\"" << value_pred.first << "\"";
+
+      // close node
+      if (node_id < tree.NumAllocatedNodes() - 1)
+        predictions << "},";
+      else
+        predictions << "}";
     }
     predictions << "]";
 
@@ -169,6 +107,40 @@ int main(int argc, char **argv) {
       predictions << "\n";
     }
   }
+}
 
-  // TODO: develop API which accepts an AST and returns a prediction
+int main(int argc, char **argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  // initialize TGen programs
+  StringSet ss;
+  TCondLanguage lang(&ss);
+  TGenProgram tgen_program_types, tgen_program_values;
+  TGen::LoadTGen(&lang, &tgen_program_types, FLAGS_tgen_program_types);
+  TGen::LoadTGen(&lang, &tgen_program_values, FLAGS_tgen_program_values);
+
+  // Loading Training data
+  std::vector<TreeStorage> trees;
+  LOG(INFO) << "Loading training data...";
+  ParseTreesInFileWithParallelJSONParse(&ss, FLAGS_training_data.c_str(), 0,
+                                        FLAGS_num_training_asts, true, &trees);
+  LOG(INFO) << "Training data with " << trees.size() << " trees loaded.";
+
+  // Training the HOG for types with predefined TCOND-program
+  LOG(INFO) << "Training types ...";
+  TGenModel model_types(tgen_program_types, true);
+  model_types.train(ss, trees, FLAGS_num_training_asts);
+  model_types.GenerativeEndTraining();
+  LOG(INFO) << "Training types done.";
+
+  // Training the HOG for values with predefined TCOND-program
+  LOG(INFO) << "Training values ...";
+  TGenModel model_values(tgen_program_values, false);
+  model_values.train(ss, trees, FLAGS_num_training_asts);
+  model_values.GenerativeEndTraining();
+  LOG(INFO) << "Training values done.";
+
+  // write predictions from source file into target file
+  write_pred_to_file(model_types, model_values, ss, 1, FLAGS_source_file,
+      FLAGS_target_file);
 }
