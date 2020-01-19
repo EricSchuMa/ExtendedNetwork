280ed9b7994a7ff7ed42656acd40cc2d58c90eb8
KimTuyenLe <lethikimtuyen87@gmail.com>
2020-01-19 19:50:45 +0100

    Update prediction_viewer (write log file, fix open file, read list of positions and predictions from csv)

--- AST2json/prediction_viewer.py
+++ AST2json/prediction_viewer.py
@@ -1,9 +1,37 @@
 import json as json
 import re
 from parse_python_v2 import parse_file
+import pandas as pd
 
 train_data = "D:\PythonProjects\AST2json\python100k_train.txt"
 test_data = "D:\PythonProjects\AST2json\python50k_eval.txt"
+csv_file = "D:\\PythonProjects\\AST2json\\2020-01-18-1515-results_log-sorted-by-file_id.csv"
+log_file = "./result/summary.txt"
+
+
+def create_lists(data, file_ids, num_file):
+    positions = []
+    predictions = []
+
+    for i in range(num_file):
+        file_id = file_ids[i]
+        is_file_id = data["file_id"] == file_id
+        file_data = data[is_file_id]
+        temp_data = file_data[["new_prediction", "file_id", "src_line", "ast_node_idx"]]
+        for idx, row in temp_data.iterrows():
+            positions.append((row["file_id"], row["src_line"], row["ast_node_idx"]))
+            if row["new_prediction"] == 0:
+                prediction = "HOG"
+            elif row["new_prediction"] == 1:
+                prediction = "UNK"
+            elif row["new_prediction"] == 2:
+                prediction = "PRED=LABEL"
+            else:
+                prediction = "OTHER"
+
+            predictions.append(prediction)
+
+    return positions, predictions
 
 
 def preprocess(poss, predicts):
@@ -52,6 +80,7 @@ def modify_line(str, predict_info, json_ast, line_no):
                 else:
                     # TODO
                     # Assuming this case will not be run
+                    traced_value.append(dic["type"])
                     value_predict.append((dic["type"], prediction, traced_value.count(dic["type"])))
             else:
                 _, linenum, _ = dic["location"]
@@ -62,10 +91,13 @@ def modify_line(str, predict_info, json_ast, line_no):
     for member in value_predict:
         val, predict, order = member
         if val in modified_line:
-            occurred_value = [m.start() for m in re.finditer(val, modified_line)]
-            insert_index = occurred_value[order-1]
-            modified_line = modified_line[:insert_index] + "|" + predict + "| " + modified_line[insert_index:]
-
+            try:
+                occurred_value = [m.start() for m in re.finditer(val, modified_line)]  # does not work well if val is regex
+                if len(occurred_value):
+                    insert_index = occurred_value[order-1]
+                    modified_line = modified_line[:insert_index] + "|" + predict + "| " + modified_line[insert_index:]
+            except:
+                pass
     return modified_line
 
 
@@ -76,10 +108,10 @@ def prediction_viewer(poss, predicts):
             line_info = pos_predict[fid]
             linenos = line_info.keys()
             file_name = get_file_name(fid)
-            json_ast = parse_file(file_name, fid)
+            json_ast = parse_file(file_name, 0)  # cheat code, cannot run with file id numpy.int64
             target_file = "./result/prediction_result_"+str(fid)+".py"
-            with open(file_name) as fsource:
-                with open(target_file, "w") as fresult:
+            with open(file_name, "r") as fsource:
+                with open(target_file, "a+") as fresult:
                     idx = 0
                     for line in fsource:
                         idx += 1
@@ -89,13 +121,29 @@ def prediction_viewer(poss, predicts):
 
                         fresult.write(new_line)
             print("Affected lines of file {}: {}".format(fid, linenos))
+            with open(log_file, "a+") as flog:
+                flog.write("Affected lines of file {}: {}".format(fid, linenos))
+                flog.write("\n")
+                flog.write("\n")
     else:
         print("Missing information! Two lists don't have the same length.")
 
 
 if __name__ == "__main__":
-    positions = [[0, 28, 23], [0, 28, 26], [0, 28, 27], [0, 30, 37]]
-    predictions = ["UNK", "HOG", "PTN", "EN"]
+    # positions = [[0, 28, 23], [0, 28, 26], [0, 28, 27], [0, 30, 37], [0, 35, 41]]
+    # predictions = ["UNK", "HOG", "PTN", "EN", "EN"]
+    data = pd.read_csv(csv_file)
+    is_not_padded = data["file_id"] != -999
+    filtered_data = data[is_not_padded]
+    file_ids = filtered_data["file_id"].unique().tolist()
+
+    positions, predictions = create_lists(filtered_data, file_ids, 10)
+
+    print(positions)
+    print(predictions)
+    print(len(positions))
+    print(len(predictions))
+
     prediction_viewer(positions, predictions)
 
 
